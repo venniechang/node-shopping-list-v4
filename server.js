4ca7907d669ef1880d51a16b1fc0ce88d8cee433
Vennie Chang <venniechang@yahoo.com>
2018-04-12 21:58:03 -0700

    added working put command

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
     }
   }
 
+
   if (req.params.id !== req.body.id) {
     const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
     console.error(message);
@@ -76,6 +77,33 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   });
   res.status(204).end();
 });
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipe \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).end();
+});
+
 
 // when DELETE request comes in with an id in path,
 // try to delete that item from ShoppingList.
@@ -91,8 +119,7 @@ app.get('/recipes', (req, res) => {
 });
 
 app.post('/recipes', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'ingredients'];
+  const requiredFields = ['name', 'ingredients', ];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
